b831cfe746ad1c185917a0ec486cd7137ab0d909
Georg Thierry <georg.delacruzacho@gmail.com>
2024-11-04 19:11:25 -0500

    Tercer Commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -6,6 +6,7 @@ public class JavaApplication1 {
 
 
     public static void main(String[] args) {
+        Persona p1 = new Persona();
         
         System.out.println("Hola, mundo.");
         System.out.println("HOLAAA");
