7f7389dd586389193ee98a1e9496c12b577de34b
Georg Thierry <georg.delacruzacho@gmail.com>
2024-11-04 19:03:11 -0500

    segundo commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -8,6 +8,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         
         System.out.println("Hola, mundo.");
+        System.out.println("HOLAAA");
         
     }
     
